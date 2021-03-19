df573145125074c6695eac79adcd163156a78236
Leonardo Norambuena <leonardo.norambuenam@gmail.com>
2021-03-18 21:15:05 -0300

    Primera clase

---
+++ Ejecicio1/Ejecicio1/Controllers/ProductsController.cs
@@ -0,0 +1,58 @@
+﻿using Ejecicio1.Models.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace Ejecicio1.Controllers
+{
+    public class ProductsController : Controller
+    {
+        List<Product> Products;
+        public ProductsController()
+        {
+            Products = new List<Product>();
+            Products.Add(new Product {
+                Id = 1,
+                Name = "Zelda",
+                Image = "https://i.pinimg.com/originals/0f/63/61/0f6361a920f327e30b5de2e03514ede8.jpg",
+                Price = 25000,
+                Stock = 2
+            });
+
+
+            Products.Add(new Product
+            {
+                Id = 2,
+                Name = "Mario Bros",
+                Image = "https://www.mariowiki.com/images/a/a7/MarioBoss365.jpeg",
+                Price = 27000,
+                Stock = 4
+            });
+
+        }
+        // GET: Products
+        public ActionResult Index()
+        {
+            return View(Products);
+        }
+        // HttpGet esto no lo ve el usuario
+        public ActionResult Show(int? id)
+        {
+            if (id == null) return RedirectToAction("NotFound", "Inicio");
+
+            Product product = Products.FirstOrDefault(x => x.Id == (int)id);
+            if (product == null)
+            {
+                TempData["ErrorMessage"] = "El producto no fue encontrado";
+                return RedirectToAction("NotFound", "Inicio");
+            }
+                
+
+
+            return View(product);
+        }
+    }
+}
---
+++ Ejecicio1/Ejecicio1/Models/Data/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ejecicio1.Models.Data
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Image { get; set; }
+
+        public int Price { get; set; }
+
+        public int Stock { get; set; }
+
+        public int Total => Price * Stock;
+    }
+}
